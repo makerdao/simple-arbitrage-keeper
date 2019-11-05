76dd47cb804e73f746bd4622b33afd4af57f6219
Kenton Prescott <kentonprescott@gmail.com>
2019-11-05 15:55:28 -0800

    Update simple_arbitrage_keeper.py

--- simple_arbitrage_keeper/simple_arbitrage_keeper.py
+++ simple_arbitrage_keeper/simple_arbitrage_keeper.py
@@ -1,6 +1,6 @@
 # This file is part of Maker Keeper Framework.
 #
-# Copyright (C) 2017-2018 reverendus
+# Copyright (C) 2017-2018 reverendus, kentonprescott
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
